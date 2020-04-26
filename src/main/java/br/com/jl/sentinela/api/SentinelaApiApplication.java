30670900ca2787d4b0f782c1ac66b0a25683bd9b
Juan Leiro <juan.leiro@gmail.com>
2020-04-26 14:13:11 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/br/com/jl/sentinela/api/SentinelaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jl.sentinela.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelaApiApplication.class, args);
+	}
+
+}
